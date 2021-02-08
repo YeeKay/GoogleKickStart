5c56eaf0c10507de3808e6035bae7c8d244552cd
Yee Kay Tan <48430466+YeeKay@users.noreply.github.com>
2021-02-08 19:55:15 +1100

    Update RPN.py

--- Round A - Google Kickstart 2013/RPN.py
+++ Round A - Google Kickstart 2013/RPN.py
@@ -1,8 +1,14 @@
+# I do not own the Rights to the following code
+# @author : Harmon758 
+# Link to source code: 
+# https://github.com/Harmon758/Google-Kick-Start/blob/master/2013/Round%20A/Problem%20A/A-solution.py
+
 number_to_word = {0: "zero", 1: "one", 2: "two", 3: "three", 4: "four",
                   5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"}
 successive_word = {2: "double", 3: "triple", 4: "quadruple", 5: "quintuple",
                    6: "sextuple", 7: "septuple", 8: "octuple", 9: "nonuple",
                    10: "decuple"}
+
 
 T = int(input())
 for x in range(1, T + 1):
