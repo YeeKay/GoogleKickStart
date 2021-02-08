e580af46b63d23fc89a906410e4fc6572f4de825
Yee Kay Tan <48430466+YeeKay@users.noreply.github.com>
2021-02-08 19:42:54 +1100

    Update Sorting.py

--- Round A - Google Kickstart 2013/Sorting.py
+++ Round A - Google Kickstart 2013/Sorting.py
@@ -2,6 +2,7 @@ import sys
 T=int (input())
 for x in range (1, T + 1):
     Number_Of_Book = int(input())
+    #map input into a list
     inp = list(map(int,input().split()))
     odd=[]
     even=[]
@@ -13,8 +14,11 @@ for x in range (1, T + 1):
         else :
             odd.append(i)
             continue
+    #sort odd numbers in ascending order while
+    #even numbers in descending order
     odd_iterator=iter(odd.sort())
     even_iterator=iter(even.sort(reverse=True))
+    #match odd/even numbers from sorted list based on original input 
     for a in inp:
         if ( a % 2 ) == 0:
             output.append(str(next(even_iterator)))
@@ -22,5 +26,7 @@ for x in range (1, T + 1):
         else:
             output.append(str(next(odd_iterator)))
             continue
+    #takes all items in an iterable and joins them into one string (output)
+    #where ' ' is the separator 
     output=' '.join(output)
     print(f"Case #{x}: {output}")
