81f848f041ff0a8d8f6642d617670ce82554fa1e
Yee Kay Tan <48430466+YeeKay@users.noreply.github.com>
2021-02-08 20:29:43 +1100

    Update SC.py

--- Round B - Google Kickstart 2013/SC.py
+++ Round B - Google Kickstart 2013/SC.py
@@ -1,13 +1,16 @@
-/* Copyright © 2020 Yee Kay Tan - All Rights Reserved
- */
-/** The following code passes Test Set 1 and Test Set 2 */ 
+# Copyright © 2020 Yee Kay Tan - All Rights Reserved
+# The following code passes Test Set 1 and Test Set 2  
+
 import numpy as np
 import sys
+#sort all numbers in a column and row and cross-check it with a sorted list from 1.. dimension 
 def check_column_row(matrix,dimension):
     for b in range(0,dimension):
         if sorted(matrix[b])!=list(range(1,dimension+1)) or sorted(matrix[:,b])!=list(range(1,dimension+1)):
             return False
-
+        
+#slicing sudoku into square of n*n, flatten and sort it and cross-check it with a
+#sorted list from 1 .. dimension 
 def check_box(rowstart,rowend,columnstart,columnend,size,dimension,matrix):
     while rowend<=dimension:
         while columnend<=dimension:
@@ -23,12 +26,14 @@ def check_box(rowstart,rowend,columnstart,columnend,size,dimension,matrix):
 
 num_of_test_case=int(input())
 for x in range(1,num_of_test_case+1):
+    #building sudoku 
     matrix=[]
     size=int(input())
     dimension = size * size
     for a in range(1,dimension+1):
         row = list(map(int,input().split()))
-        matrix.append(row)  
+        matrix.append(row)
+    #convert sudoku into numpy array for easy processing 
     matrix=np.array(matrix)
     #check column
     if (check_column_row(matrix,dimension)==False):
